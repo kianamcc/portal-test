b3594406572766d085e63b5c18da60fb28c98f51
Kiana McCullough <kmccullough@w259.home>
2024-06-07 11:20:16 -0700

    some cleanup

--- apps/SageAccountWeb/src/Constants.ts
+++
@@ -1,2 +0,0 @@
-export const RESET_2FA_ROUTE = '/reset2FA'
-export const RESET_2FA_SIGNED_TOKEN_PARAM = 'twoFAResetToken'
--- apps/synapse-oauth-signin/src/types.d.ts
+++
@@ -1,2 +0,0 @@
-// Import MUI type augmentations from 'synapse-react-client' so we can use custom property values defined for our MUI theme
-import 'synapse-react-client/ThemeTypes'
